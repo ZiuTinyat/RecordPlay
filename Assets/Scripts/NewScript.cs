68389dec76f5b480c217b7b66893db49e0fe23a6
zty <ec,mikezhao@gmail.com>
2016-05-16 15:17:21 +0800

    + gitignore

---
+++ Assets/Scripts/NewScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewScript : MonoBehaviour {
+
+	public int a = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
